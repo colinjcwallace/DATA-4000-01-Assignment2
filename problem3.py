698164c8f103c11de8f2850e912a538363775292
colinjcwallace <colin.wallace@student.fairfield.edu>
2026-02-09 18:11:12 -0500

    problem 3

---
+++ problem3.py
@@ -0,0 +1,14 @@
+def main():
+    credit_score = int(input("What is your credit score? "))
+    income = float(input("What is your annual income? "))
+
+    if credit_score >= 720 and income >= 60000:
+        risk = "low"
+    elif credit_score >= 650 and income >= 40000:
+        risk = "medium"
+    else:
+        risk = "high"
+    
+    print(f"Loan Risk Category: {risk}")
+
+main()
